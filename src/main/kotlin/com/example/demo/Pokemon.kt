af3aee70057184d29590e13ea523a6cb04b3e9b8
Carlos <carlostena@centroafuera.onmicrosoft.com>
2022-01-20 11:04:58 +0100

    Updated

--- src/main/kotlin/com/example/demo/Pokemon.kt
+++ src/main/kotlin/com/example/demo/Pokemon.kt
@@ -1,15 +1,10 @@
 package com.example.demo
 
 import com.google.gson.Gson
-import com.google.gson.JsonObject
-import netscape.javascript.JSObject
 
 data class Pokemon(var nombre: String, var nivel: Int) {
     override fun toString(): String {
         val gson = Gson()
-        val json = gson.toJson(this)
-        val jsonObject = JsonObject()
-        jsonObject.
         return gson.toJson(this)
     }
 }
